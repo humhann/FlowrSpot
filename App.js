779415ac08a44cd9fde1c6075133ca414550d024
Gregor Hann <gregor585@gmail.com>
2018-05-20 12:38:31 +0200

    Cleanup

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-import HomeScreen from './app/components/HomeScreen';
+import HomeScreen from './app/components/HomeScreen/HomeScreen';
 
 export default class App extends React.Component {
   render() {
