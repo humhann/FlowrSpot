c85227fa509422db95ad3e2b79f11f34b7835351
Gregor Hann <gregor585@gmail.com>
2018-05-20 16:53:34 +0200

    Add & load Ubuntu fonts

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import {
   createStackNavigator,
   createBottomTabNavigator,
 } from 'react-navigation';
+import { Font } from 'expo';
 
 import HomeScreen from './app/components/HomeScreen/HomeScreen';
 import Tab2Screen from './app/components/Tab2Screen/Tab2Screen';
@@ -10,9 +11,30 @@ import Tab3Screen from './app/components/Tab3Screen/Tab3Screen';
 import Tab4Screen from './app/components/Tab4Screen/Tab4Screen';
 
 export default class App extends React.Component {
+  state = {
+    fontsLoaded: false,
+  };
+
+  async componentDidMount() {
+    /* eslint-disable global-require */
+    await Font.loadAsync({
+      'ubuntu-bold': require('./app/assets/fonts/Ubuntu-Bold.ttf'),
+      'ubuntu-bold-italic': require('./app/assets/fonts/Ubuntu-BoldItalic.ttf'),
+      'ubuntu-italic': require('./app/assets/fonts/Ubuntu-Italic.ttf'),
+      'ubuntu-light': require('./app/assets/fonts/Ubuntu-Light.ttf'),
+      'ubuntu-light-italic': require('./app/assets/fonts/Ubuntu-LightItalic.ttf'),
+      'ubuntu-medium': require('./app/assets/fonts/Ubuntu-Medium.ttf'),
+      'ubuntu-medium-italic': require('./app/assets/fonts/Ubuntu-MediumItalic.ttf'),
+      'ubuntu-regular': require('./app/assets/fonts/Ubuntu-Regular.ttf'),
+    });
+    /* eslint-enable global-require */
+
+    this.setState({ fontsLoaded: true });
+  }
+
   render() {
     return (
-      <AppNavigator />
+      this.state.fontsLoaded && <AppNavigator />
     );
   }
 }
